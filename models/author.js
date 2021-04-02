518f96ee0bcf742ddc14b0b0e09d67c2f9dbbfcf
Bigice-spec <kenbigice2015@gmail.com>
2021-04-02 15:50:14 +0300

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  //return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  let dateBirth = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : " ";
+  let dateDeath = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : " ";
+
+  return `${dateBirth} - ${dateDeath}`
+
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
